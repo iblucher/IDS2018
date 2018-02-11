6eb9ad5fba252c71522e87f040a4c32e623155a2
Isabela Blucher <isabelablucher@hotmail.com>
2018-02-11 17:36:03 +0100

    Finished exercise 2

--- Assignment 1/assignment.py
+++ Assignment 1/assignment.py
@@ -4,4 +4,26 @@ import meanFEV1 as mf
 
 data = np.loadtxt('smoking.txt')
 
-mf.meanFEV1(data)
+[row, col] = np.shape(data)
+print(row)
+print(col)
+
+# Separate datset in smokers and non-smokers
+nonsmokers = []
+smokers = []
+for i in range(row):
+    if data[i, 4] == 0:
+        nonsmokers.append(data[i, :])
+    else:
+        smokers.append(data[i, :])
+smokers = np.array(smokers)
+nonsmokers = np.array(nonsmokers)
+
+(smean, nmean) = mf.meanFEV1(smokers, nonsmokers)
+
+# Boxplot of the two FEV1 scores
+labels = ['Smokers', 'Non-smokers']
+plt.boxplot([smokers[:, 1], nonsmokers[:, 1]], labels = labels)
+plt.title('Boxplot of FEV1 scores for smokers and non smokers')
+plt.ylabel('FEV1 score')
+plt.show()
--- Assignment 1/meanFEV1.py
+++ Assignment 1/meanFEV1.py
@@ -3,27 +3,11 @@
 
 import numpy as np
 
-def meanFEV1(data):
-	[row, col] = np.shape(data)
-	print(row)
-	print(col)
+def meanFEV1(smokers, nonsmokers):
 
-	# SEPARAR O DATASET ENTRE FUMANTES E NAO-FUMANTES (quinta coluna)
-	nonsmokers = []
-	smokers = []
-	for i in range(row):
-	    if data[i, 4] == 0:
-	        nonsmokers.append(data[i, :])
-	    else:
-	        smokers.append(data[i, :])
-	smokers = np.array(smokers)
-	nonsmokers = np.array(nonsmokers)
-
-	# CALCULAR O FEV1 MEDIO PRA CADA MATRIZ
-	[sr, sc] = np.shape(smokers)
-	[nr, nc]  = np.shape(nonsmokers)
+	# Calculate the average FEV1 score for each group
 	smean = smokers[:, 1].mean()
 	nmean = nonsmokers[:, 1].mean()
 	print(smean)
 	print(nmean)
-	
+	return(smean, nmean)

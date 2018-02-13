454784d9bb2df555059a8441ce8f07f67851a8c8
Isabela Blucher <isabelablucher@hotmail.com>
2018-02-13 11:46:00 +0100

    Begin exercise 3 and add comments

--- Assignment 1/assignment.py
+++ Assignment 1/assignment.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import meanFEV1 as mf
+import hyptest as hp
 
+# Read the dataset from file
 data = np.loadtxt('smoking.txt')
 
+# Store dimensions of dataset
 [row, col] = np.shape(data)
 print(row)
 print(col)
@@ -19,11 +22,21 @@ for i in range(row):
 smokers = np.array(smokers)
 nonsmokers = np.array(nonsmokers)
 
+# Store dimensions of subgroups
+[srow, scol] = np.shape(smokers)
+[nrow, ncol] = np.shape(nonsmokers)
+print(srow)
+print(nrow)
+
+# Calculate means of FEV1 scores for both groups
 (smean, nmean) = mf.meanFEV1(smokers, nonsmokers)
 
-# Boxplot of the two FEV1 scores
+# Plot boxplot of the two FEV1 scores
 labels = ['Smokers', 'Non-smokers']
 plt.boxplot([smokers[:, 1], nonsmokers[:, 1]], labels = labels)
 plt.title('Boxplot of FEV1 scores for smokers and non smokers')
 plt.ylabel('FEV1 score')
 plt.show()
+
+# Make hypothesis test on the means
+hp.hyptest(smokers, nonsmokers)
--- Assignment 1/hyptest.py
+++ Assignment 1/hyptest.py
@@ -1,5 +1,20 @@
 
 # data is the datamatrix of the smoking dataset, e.g. as obtained by data = numpy.loadtxt('smoking.txt')
 # should return True if the null hypothesis is rejected and False otherwise, i.e. return p < 0.05
-def hyptest(data):
-	pass
+import numpy as np
+from scipy.stats import ttest_ind, ttest_ind_from_stats
+
+def hyptest(smokers, nonsmokers):
+	[srow, scol] = np.shape(smokers)
+	[nrow, ncol] = np.shape(nonsmokers)
+
+	(t, p) = ttest_ind(smokers[:, 1], nonsmokers[:, 1], equal_var = False)
+	svar = smokers[:, 1].var()
+	nvar = nonsmokers[:, 1].var()
+
+	s = svar/srow
+	n = nvar/ncol
+	den = np.sqrt(s + n)
+	tt = (smokers[:, 1].mean() - nonsmokers[:, 1].mean())/den
+	print(tt)
+	print("ttest_ind:            t = %g  p = %g" % (t, p))

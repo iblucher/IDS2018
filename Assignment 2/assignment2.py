451f7f3b39fa07c712aae5f479e7f6026e737efb
Isabela Blucher <isabelablucher@hotmail.com>
2018-02-27 17:38:45 +0100

    Start cross-validation in exercise 2

--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, zero_one_loss
+from sklearn.model_selection import KFold
 
 # read training and test datasets
 dataTrain = np.loadtxt('IDSWeedCropTrain.csv', delimiter = ',')
@@ -17,4 +18,20 @@ nn = KNeighborsClassifier(n_neighbors = 1)
 nn.fit(xTrain, yTrain)
 
 accTest = accuracy_score(yTest, nn.predict(xTest))
-print(accTest)
+#print(accTest)
+
+# cross-validation and hyperparameter selection
+knn = KNeighborsClassifier(n_neighbors = 1)
+
+cv = KFold(n_splits = 5)
+loss = []
+for train_index, test_index in cv.split(xTrain):
+    xTrainCV, xTestCV, yTrainCV, yTestCV = xTrain[train_index], xTrain[test_index], yTrain[train_index], yTrain[test_index]
+    print(knn.fit(xTrainCV, yTrainCV).score(xTestCV, yTestCV))
+    loss.append(zero_one_loss(yTestCV, knn.predict(xTestCV)))
+
+loss = np.array(loss)
+print(loss)
+
+avg_loss = np.mean(loss, dtype = np.float64)
+print(avg_loss)

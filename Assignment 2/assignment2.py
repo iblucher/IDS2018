3bc44a5f9cafb2e5542290db94d2049255dd76d1
Isabela Blucher <isabelablucher@hotmail.com>
2018-02-27 20:42:55 +0100

    Finish exercises 2 and 3

--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, zero_one_loss
 from sklearn.model_selection import KFold
+from sklearn import preprocessing
 
 # read training and test datasets
 dataTrain = np.loadtxt('IDSWeedCropTrain.csv', delimiter = ',')
@@ -14,24 +15,26 @@ xTest = dataTest[:, :-1]
 yTest = dataTest[:, -1]
 
 # initialize nearest neighbor classifier and fit the training data
-nn = KNeighborsClassifier(n_neighbors = 1)
+nn = KNeighborsClassifier(n_neighbors = 3)
 nn.fit(xTrain, yTrain)
 
 accTest = accuracy_score(yTest, nn.predict(xTest))
-#print(accTest)
+print(accTest)
 
 # cross-validation and hyperparameter selection
-knn = KNeighborsClassifier(n_neighbors = 1)
+avg_loss = []
+for i in [1, 3, 5, 7, 9, 11]:
+    knn = KNeighborsClassifier(n_neighbors = i)
 
-cv = KFold(n_splits = 5)
-loss = []
-for train_index, test_index in cv.split(xTrain):
-    xTrainCV, xTestCV, yTrainCV, yTestCV = xTrain[train_index], xTrain[test_index], yTrain[train_index], yTrain[test_index]
-    print(knn.fit(xTrainCV, yTrainCV).score(xTestCV, yTestCV))
-    loss.append(zero_one_loss(yTestCV, knn.predict(xTestCV)))
+    cv = KFold(n_splits = 5)
+    loss = []
+    for train_index, test_index in cv.split(xTrain):
+        xTrainCV, xTestCV, yTrainCV, yTestCV = xTrain[train_index], xTrain[test_index], yTrain[train_index], yTrain[test_index]
+        knn.fit(xTrainCV, yTrainCV).score(xTestCV, yTestCV)
+        loss.append(zero_one_loss(yTestCV, knn.predict(xTestCV)))
 
-loss = np.array(loss)
-print(loss)
+    loss = np.array(loss)
+    avg_loss.append(np.mean(loss, dtype = np.float64))
 
-avg_loss = np.mean(loss, dtype = np.float64)
+avg_loss = np.array(avg_loss)
 print(avg_loss)

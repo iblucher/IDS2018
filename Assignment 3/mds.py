0291edfc4132d7c06be789e76a607046cc53cd9e
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-06 10:15:29 +0100

    Start assignment 3

---
+++ Assignment 3/mds.py
@@ -0,0 +1,6 @@
+# input:   1) datamatrix as loaded by numpy.loadtxt('dataset.txt')
+#	   2) an integer d specifying the number of dimensions for the output (most commonly used are 2 or 3)
+# output:  1) an N x d numpy array containing the d coordinates of the N original datapoints projected onto the top d PCs
+#
+def mds(data, d):
+    pass
---
+++ Assignment 3/pca.py
@@ -0,0 +1,7 @@
+# input: datamatrix as loaded by numpy.loadtxt('dataset.txt')
+# output:  1) the eigenvalues in a vector (numpy array) in descending order
+#          2) the unit eigenvectors in a matrix (numpy array) with each column being an eigenvector (in the same order as its associated eigenvalue)
+#
+# note: make sure the order of the eigenvalues (the projected variance) is decreasing, and the eigenvectors have the same order as their associated eigenvalues
+def pca(data):
+    pass

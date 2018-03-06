80fcf4799907f1be564b01925f0f6ec5118f82b5
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-06 21:23:08 +0100

    Fix PCA matrix dimensions

--- Assignment 3/pca.py
+++ Assignment 3/pca.py
@@ -3,22 +3,33 @@
 #          2) the unit eigenvectors in a matrix (numpy array) with each column being an eigenvector (in the same order as its associated eigenvalue)
 #
 # note: make sure the order of the eigenvalues (the projected variance) is decreasing, and the eigenvectors have the same order as their associated eigenvalues
+from __future__ import division
 import numpy as np
-import numpy.matlib as nmat
 
 def pca(data):
+    data = data.transpose()
     [M, N] = data.shape
 
     # center data
-    mean = np.mean(data, axis = 1)
+    mean = np.mean(data, axis = 0)
     for i in range(M):
         data[i, :] = data[i, :] - mean[i]
     #print(data)
 
     # find covariance matrix
-    cov = np.matmul(data, data.transpose())
+    datat = data.transpose()
+    cov = np.dot(data, datat)
     cov = np.array(cov)
-    cov = 1 / (N - 1) * cov
-    print(cov)
+    cov_mat = 1 / N * cov
+    #print(cov_mat)
 
-    
+    # find eigenvectors and eigenvalues
+    (evals, evecs) = np.linalg.eig(cov_mat)
+    print(evals)
+    print(evecs)
+
+    # confirm eigenvectors have unit length 1
+    for ev in evecs:
+        np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+
+    # sorting eigenvectors by decreasing eigenvalues

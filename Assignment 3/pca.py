c891be73652ac696a7b73c305b2cd766085313f7
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-06 21:59:07 +0100

    Finish exercise 1

--- Assignment 3/pca.py
+++ Assignment 3/pca.py
@@ -7,6 +7,7 @@ from __future__ import division
 import numpy as np
 
 def pca(data):
+    # M stands for number of dimensions and N stands for number of trials
     data = data.transpose()
     [M, N] = data.shape
 
@@ -25,11 +26,28 @@ def pca(data):
 
     # find eigenvectors and eigenvalues
     (evals, evecs) = np.linalg.eig(cov_mat)
-    print(evals)
-    print(evecs)
+    evals = np.array(evals)
+    #print(evals)
+    #print(evecs)
 
     # confirm eigenvectors have unit length 1
     for ev in evecs:
         np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
 
     # sorting eigenvectors by decreasing eigenvalues
+    eig_pairs = []
+    for i in range(len(evals)):
+        eig_pairs.append((evals[i], evecs[:, i]))
+    eig_pairs.sort(key = lambda x: x[0], reverse = True)
+
+    evals_sorted = []
+    evecs_sorted = []
+    for i in range(len(evals)):
+        evals_sorted.append(eig_pairs[i][0])
+        evecs_sorted.append(eig_pairs[i][1])
+    evals_sorted = np.array(evals_sorted)
+    evecs_sorted = np.array(evecs_sorted)
+    # this transposition is so that the eigenvectors are the column and not the row vectors of the evecs_sorted matrix
+    evecs_sorted = evecs_sorted.transpose()
+
+    return (evals_sorted, evecs_sorted)

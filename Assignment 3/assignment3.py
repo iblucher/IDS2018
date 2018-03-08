ca27f5506d9e74b4b0700747acc576e6fb6257ca
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-08 14:59:57 +0100

    Exercise 1b and 1c plots

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -4,27 +4,42 @@ from pca import pca
 
 murder_data = np.loadtxt('murderdata2d.txt')
 
-(evals, evecs) = pca(murder_data)
+(evals_murder, evecs_murder) = pca(murder_data)
 
 # scatter plot for murder dataset
 # x is the percent unemployed and y is the murders per annum per 1000000 inhabitants
 x = murder_data[:, 0]
 y = murder_data[:, 1]
-#print(x)
-#print(y)
 plt.scatter(x,y)
-plt.title("Scatter plot of the murder data")
+plt.title("Scatter plot of the centered murder data")
 plt.xlabel("Percent unemployed")
 plt.ylabel("Murders per annum per 1,000,000 inhabitants")
+#plt.axis('equal')
+
+s0 = np.sqrt(evals_murder[0])
+s1 = np.sqrt(evals_murder[1])
+plt.plot(0, 0, marker = 'x', markersize = 7, color='k', label = 'Mean', linestyle = 'None')
+plt.plot([0, s0*evecs_murder[0,0]], [0, s0*evecs_murder[1,0]], 'r', label = 'Eigenvectors')
+plt.plot([0, s1*evecs_murder[0,1]], [0, s1*evecs_murder[1,1]], 'r')
 plt.axis('equal')
-
-s0 = np.sqrt(evals[0])
-s1 = np.sqrt(evals[1])
-plt.plot([0, s0*evecs[0,0]], [0, s0*evecs[1,0]], 'r')
-plt.plot([0, s1*evecs[0,1]], [0, s1*evecs[1,1]], 'r')
-
+plt.legend()
 plt.show()
-
-
+plt.close()
 
 # scatter plot for crop dataset
+pest_data_train = np.loadtxt('IDSWeedCropTrain.csv', delimiter = ',')
+pest_data_test = np.loadtxt('IDSWeedCropTest.csv', delimiter = ',')
+pest_x_train = pest_data_train[:, :-1]
+pest_y_train = pest_data_train[:, -1]
+pest_x_test = pest_data_test[:, :-1]
+pest_y_test = pest_data_test[:, -1]
+
+(evals_pest, evecs_pest) = pca(pest_x_train)
+print(evals_pest)
+#print(evecs_pest.shape)
+
+plt.plot(evals_pest)
+plt.xlabel('PCs index')
+plt.ylabel('Projected variance')
+plt.title('Variance versus principal component index in descending order')
+plt.show()

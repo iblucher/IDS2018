77e00d13e717e07d36da1b69d024eb76c91037e5
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-15 15:36:18 +0100

    Add cell shapes and start exercise 2

--- Assignment 4/assignment4.py
+++ Assignment 4/assignment4.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pca import pca
 
 data_diatoms = np.loadtxt('diatoms.txt')
 r, c = data_diatoms.shape
@@ -9,12 +10,15 @@ r, c = data_diatoms.shape
 diatoms_x = []
 diatoms_y = []
 for i in range(0, c - 1, 2):
-    diatoms_x.append(data_diatoms[1, i])
-    diatoms_y.append(data_diatoms[1, i + 1])
-plt.scatter(diatoms_x, diatoms_y, marker = '.')
-plt.plot(diatoms_x, diatoms_y)
+    diatoms_x.append(data_diatoms[0, i])
+    diatoms_y.append(data_diatoms[0, i + 1])
+plt.scatter(diatoms_x, diatoms_y, marker = '.', color = 'b')
+plt.plot(diatoms_x, diatoms_y, 'b')
+plt.plot([diatoms_x[0], diatoms_x[89]], [diatoms_y[0], diatoms_y[89]], 'b')
 plt.axis('equal')
-plt.show()
+plt.xlabel('x-coordinates')
+plt.ylabel('y-coordinates')
+#plt.show()
 
 # plot all cells on top of each other
 for i in range(r):
@@ -25,5 +29,27 @@ for i in range(r):
         diatoms_y.append(data_diatoms[i, j + 1])
     plt.scatter(diatoms_x, diatoms_y, marker = '.')
     plt.plot(diatoms_x, diatoms_y)
+plt.xlabel('x-coordinates')
+plt.ylabel('y-coordinates')
+plt.axis('equal')
+#plt.show()
+
+# Exercise 2
+evals, evecs, mean = pca(data_diatoms)
+s0 = np.sqrt(evals[0])
+s1 = np.sqrt(evals[1])
+s2 = np.sqrt(evals[2])
+e0 = evecs[:, 0]
+e1 = evecs[:, 1]
+e2 = evecs[:, 2]
+
+spatial_var_x = []
+spatial_var_y = []
+spatial_var = mean - 2*s0*e0
+for i in range(0, c - 1, 2):
+    spatial_var_x.append(spatial_var[i])
+    spatial_var_y.append(spatial_var[i + 1])
+plt.scatter(spatial_var_x, spatial_var_y, marker = '.')
+plt.plot(spatial_var_x, spatial_var_y)
 plt.axis('equal')
 plt.show()
---
+++ Assignment 4/pca.py
@@ -0,0 +1,47 @@
+# input: datamatrix as loaded by numpy.loadtxt('dataset.txt')
+# output:  1) the eigenvalues in a vector (numpy array) in descending order
+#          2) the unit eigenvectors in a matrix (numpy array) with each column being an eigenvector (in the same order as its associated eigenvalue)
+#
+# note: make sure the order of the eigenvalues (the projected variance) is decreasing, and the eigenvectors have the same order as their associated eigenvalues
+from __future__ import division
+import numpy as np
+
+def pca(data):
+    # M stands for number of dimensions and N stands for number of trials
+    data = data.transpose()
+    [M, N] = data.shape
+
+    # calculate mean
+    mean = np.mean(data, axis = 1)
+
+    # find covariance matrix
+    datat = data.transpose()
+    cov = np.dot(data, datat)
+    cov = np.array(cov)
+    cov_mat = 1 / N * cov
+
+    # find eigenvectors and eigenvalues
+    (evals, evecs) = np.linalg.eig(cov_mat)
+    evals = np.array(evals)
+
+    # confirm eigenvectors have unit length 1
+    for ev in evecs:
+        np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+
+    # sorting eigenvectors by decreasing eigenvalues
+    eig_pairs = []
+    for i in range(len(evals)):
+        eig_pairs.append((evals[i], evecs[:, i]))
+    eig_pairs.sort(key = lambda x: x[0], reverse = True)
+
+    evals_sorted = []
+    evecs_sorted = []
+    for i in range(len(evals)):
+        evals_sorted.append(eig_pairs[i][0])
+        evecs_sorted.append(eig_pairs[i][1])
+    evals_sorted = np.array(evals_sorted)
+    evecs_sorted = np.array(evecs_sorted)
+    # this transposition is so that the eigenvectors are the column and not the row vectors of the evecs_sorted matrix
+    evecs_sorted = evecs_sorted.transpose()
+
+    return (evals_sorted, evecs_sorted, mean)

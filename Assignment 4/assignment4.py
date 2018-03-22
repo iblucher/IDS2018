df9bef1b3fe71bd8841e81750653add5013b5b3d
Isabela Blucher <isabelablucher@hotmail.com>
2018-03-22 15:58:09 +0100

    Final submission

--- Assignment 4/assignment4.py
+++ Assignment 4/assignment4.py
@@ -21,7 +21,7 @@ plt.plot([diatoms_x[0], diatoms_x[89]], [diatoms_y[0], diatoms_y[89]], 'b')
 plt.axis('equal')
 plt.xlabel('x-coordinates')
 plt.ylabel('y-coordinates')
-#plt.show()
+plt.show()
 plt.close()
 
 # plot all cells on top of each other
@@ -36,7 +36,7 @@ for i in range(r):
 plt.xlabel('x-coordinates')
 plt.ylabel('y-coordinates')
 plt.axis('equal')
-#plt.show()
+plt.show()
 plt.close()
 
 # Exercise 2
@@ -51,9 +51,10 @@ temporal0 = [mean - 2*s0*e0, mean - s0*e0, mean, mean + s0*e0, mean + 2*s0*e0]
 temporal1 = [mean - 2*s1*e1, mean - s1*e1, mean, mean + s1*e1, mean + 2*s1*e1]
 temporal2 = [mean - 2*s2*e2, mean - s2*e2, mean, mean + s2*e2, mean + 2*s2*e2]
 
-#visualize_var(temporal0, c)
-#visualize_var(temporal1, c)
-#visualize_var(temporal2, c)
+# plot spatial variance for the first 3 PCs
+visualize_var(temporal0, c)
+visualize_var(temporal1, c)
+visualize_var(temporal2, c)
 
 # Exercise 3
 data_toy = np.loadtxt('pca_toydata.txt')
@@ -62,9 +63,12 @@ scaled_toy_x = scaled_toy[:, 0]
 scaled_toy_y = scaled_toy[:, 1]
 plt.scatter(scaled_toy_x, scaled_toy_y)
 plt.axis('equal')
+plt.xlabel('Data projected onto the first PC')
+plt.ylabel('Data projected onto the second PC')
 plt.show()
+plt.close()
 
-# Remove two last data points
+# remove two last data points
 rt, ct = data_toy.shape
 new_data_toy = data_toy[:-2, :]
 scaled_new = mds(new_data_toy, 2)
@@ -72,8 +76,10 @@ scaled_new_x = scaled_new[:, 0]
 scaled_new_y = scaled_new[:, 1]
 plt.scatter(scaled_new_x, scaled_new_y)
 plt.axis('equal')
+plt.xlabel('Data projected onto the first PC')
+plt.ylabel('Data projected onto the second PC')
 plt.show()
-
+plt.close()
 
 # Exercise 4
 data_train = np.loadtxt('IDSWeedCropTrain.csv', delimiter = ',')
@@ -83,19 +89,31 @@ y_train = data_train[:, -1]
 x_test = data_test[:, :-1]
 y_test = data_test[:, -1]
 
-starting_point = np.vstack((x_train[0, ], x_train[1, ]))
-kmeans = KMeans(n_clusters = 2, n_init = 1, init = starting_point, algorithm = 'full').fit(x_train)
-centers = kmeans.cluster_centers_
-print(centers)
-
+# project crop data onto first 2 PCs
 scaled_data = mds(x_train, 2)
-print(scaled_data.shape)
-print(y_train.shape)
 scaled_x = scaled_data[:, 0]
 scaled_y = scaled_data[:, 1]
 
-for i in range(len(scaled_x)):
-    
-plt.scatter(scaled_x, scaled_y)
+# plot projected data
+for i in range(len(y_train)):
+    if y_train[i] == 1:
+        c = 'skyblue'
+    elif y_train[i] == 0:
+        c = 'slateblue'
+    plt.scatter(scaled_x[i], scaled_y[i], color = c, marker = '.')
+
+# run kmeans clustering
+starting_point = np.vstack((x_train[0, ], x_train[1, ]))
+kmeans = KMeans(n_clusters = 2, n_init = 1, init = starting_point, algorithm = 'full').fit(x_train)
+centers = kmeans.cluster_centers_
+eva, eve, m = pca(x_train)
+eve = eve[:, :2]
+projected_centers = np.matmul(centers, eve)
+print('Exercise 4: projected cluster centers')
+print(projected_centers)
+plt.scatter(projected_centers[:, 0], projected_centers[:, 1], 75, 'r', 'x')
+plt.xlabel('Data projected onto the first PC')
+plt.ylabel('Data projected onto the second PC')
 plt.axis('equal')
 plt.show()
+plt.close()
